67557fd4ab01da1f932b05355c972e1df89275d3
Oleg Dzhimiev <oleg@elphel.com>
2017-01-13 14:03:58 -0700

    license headers

--- src/hwmon.php
+++ src/hwmon.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * @file hwmon.php
+ * @brief temperature monitor backend
+ * @copyright Copyright (C) 2017 Elphel Inc.
+ * @author Oleg Dzhimiev <oleg@elphel.com>
+ *
+ * @par <b>License</b>:
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 if (isset($_GET['cmd']))
 	$cmd = $_GET['cmd'];
